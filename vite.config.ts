9ff0f9b9e45bdd1286502fe3565c0a83dc6455de
Ulad <vladis67948@gmail.com>
2024-02-29 18:27:54 +0300

    settings: added packeges, app utils

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +7,10 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			asserts: '/src/assets',
+			constants: '/src/constants',
 			components: '/src/components',
+			services: '/src/services',
+			providers: '/src/providers',
 			hooks: '/src/hooks'
 		}
 	}
